16d9ca02ca92e36fbcfd55ec86136c76b9d36596
Danial Erfanian <erfaniiyanerfan@gmail.com>
2022-01-20 21:20:57 +0330

    Init Project

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+        Debug.Log("Test1");
+    }
+
+    void FixedUpdates()
+    {
+      Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+      rb.AddForce(movement);
+      Debug.Log("Test2");
+      Debug.Log(movementX);
+      Debug.Log(movementY);
+    }
+
+}
