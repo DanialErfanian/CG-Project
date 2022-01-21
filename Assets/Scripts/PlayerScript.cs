7cf2c805d4449bb99ef729d612c381e000824e37
Danial Erfanian <erfaniiyanerfan@gmail.com>
2022-01-21 12:42:22 +0330

    Fix frame rate

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,6 +13,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 30;
         rb = GetComponent<Rigidbody>();
     }
 
