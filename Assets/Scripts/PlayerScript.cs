1d582a6719eb564106c5b846b5d93a0840f59ce6
Danial Erfanian <erfaniiyanerfan@gmail.com>
2022-01-21 12:24:11 +0330

    Make PickUp objects eatable

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -32,11 +32,18 @@ public class PlayerScript : MonoBehaviour
 
     void FixedUpdate()
     {
-      Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-      rb.AddForce(movement * speed);
-      Debug.Log("Test2");
-      Debug.Log(movementX);
-      Debug.Log(movementY);
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+        Debug.Log("Test2");
+        Debug.Log(movementX);
+        Debug.Log(movementY);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
