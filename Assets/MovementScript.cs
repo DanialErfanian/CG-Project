7b29d9605fcf4dc37ebae589a52f88a2ada0d85e
Mahdi Erfanian <mahdi.erfanian@divar.ir>
2022-01-23 18:50:04 +0330

    Modify second level

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     public CharacterController controller; 
-
+    public Transform cameraTransform ;
     public float speed = 6f;
 
 
     // Update is called once per frame
     void Update()
     {
+        
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical"); 
         Vector3 directon  = new Vector3(horizontal, 0f, vertical).normalized;
-
+        directon = cameraTransform.TransformDirection(directon);
         if (directon.magnitude >= 0.1f){
             controller.Move(directon * speed * Time.deltaTime);
         }
