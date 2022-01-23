327495c2d7628b095ffd16d47b9c74a276ddb09c
Danial Erfanian <erfaniiyanerfan@gmail.com>
2022-01-23 17:41:02 +0330

    Add Level2 button to MainMenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -9,7 +9,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGame () { 
+    public void PlayLevel2 () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void QuitGame () {
         Application.Quit();
     }
 }
